814c1fcf1719e17ced24c4f5c7e2203ba1e5f43f
nencyd390 <nencyd390@gmail.com>
2026-01-16 16:48:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Backend  , server is running on port number 3000 , hello");
+  res.send("Hello Backend  , server is running on port number 3000 ");
 });
 
 app.listen(3000, () => {
